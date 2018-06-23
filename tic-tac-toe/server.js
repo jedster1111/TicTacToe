bb4883f137e4563113ee28f009789596892bc5ab
Jed <jedster1111@hotmail.co.uk>
2018-06-23 18:24:07 +0100

    socket and react app are talking

--- tic-tac-toe/server.js
+++ tic-tac-toe/server.js
@@ -4,6 +4,8 @@ const port = 8000;
 io.listen(port);
 console.log('listening on port ', port);
 
-io.on('connection', (client) => {
-
+io.on('connection', socket => {
+    console.log("New client connected");
+    socket.emit("connection");
+    socket.on("disconnect", ()=> console.log("Client disconnected"));
 });
--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import socketIOClient from "socket.io-client";
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:8000",
+    };
+  }
+
+  componentDidMount(){
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        Hello
       </div>
     );
   }
