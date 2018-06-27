e6e2ee659995f631103046581c1247e074d88694
Jed <jedster1111@hotmail.co.uk>
2018-06-27 01:59:45 +0100

    Created RoomList component

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -10,25 +10,20 @@ class App extends Component {
       endpoint: "http://127.0.0.1:8000",
     };
   }
-
   componentDidMount(){
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
-
-  socket.on("hello", (rooms) => {
-    console.log('we made contact!' + rooms);
-    socket.emit('set-name', 'jed');
-    socket.emit('join-room', 'testRoom');
-    socket.emit('set-team','X');
-    socket.emit('new-square', 0);
-    socket.emit('set-team', null);
-    socket.emit('new-square', 1);
-  });
-  socket.on('game-data',(data) => console.log(data));
-
+    socket.on("hello", (rooms) => {
+      console.log('we made contact!');
+      //socket.emit('set-name', 'jed');
+      //socket.emit('join-room', 'testRoom');
+      //socket.emit('set-team','X');
+      //socket.emit('new-square', 0);
+      //socket.emit('set-team', null);
+      //socket.emit('new-square', 1);
+    });
+    socket.on('game-data',(data) => console.log(data));
   }
-
-
   render() {
     return (
       <div>
@@ -36,6 +31,14 @@ class App extends Component {
       </div>
     );
   }
+}
+
+function RoomList(props){
+  return(
+    <ul>
+      {this.props.rooms.map(room => <li key = {room}>{room}</li>)};
+    </ul>
+  )
 }
 
 export default App;
