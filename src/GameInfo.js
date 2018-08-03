eec3678d134a728bb7761de93e31287502b3f181
Jed <jedster1111@hotmail.co.uk>
2018-08-03 15:17:09 +0100

    Created GameInfo.css

--- src/GameInfo.js
+++ src/GameInfo.js
@@ -1,41 +1,51 @@
 import React, {Fragment} from 'react';
 import {findBestMatch} from 'string-similarity';
 import {SingleInput} from './SingleInput';
+import './GameInfo.css';
+
+const RoomWithJoinButton = (props) => {
+  return(
+    <div key ={props.containerKey} className ={props.containerClass}>
+			{props.rooms.map((room) => {
+				const isJoinedRoom = room === props.roomNameConfirmed;
+				const containerClass = isJoinedRoom ? 'confirmed-room room-list' : 'room-list';
+				const buttonClass = isJoinedRoom ? 'confirmed' : '';
+				const buttonText = isJoinedRoom ? 'Joined' : 'Join';
+				return(
+					<div key={room} className={containerClass}>
+						<div className='room-list-text'>{room}</div>
+						<button onClick={(e)=>props.handleJoinRoomClick(e,room)} className={buttonClass}>{buttonText}</button>
+					</div>
+				)
+			})}
+		</div>
+	)
+}
 
 export const RoomList = (props) => {
   let roomList = [];
   if(Array.isArray(props.roomsFiltered) && props.roomsFiltered.length !== 0){
     roomList.push(
-      <div key='roomsFiltered' className='rooms-filtered'>
-        {props.roomsFiltered.map((room) => {
-          const containerClass = room === props.roomNameConfirmed ? 'confirmed-room room-list' : 'room-list';
-          const buttonClass = room === props.roomNameConfirmed ? 'confirmed-room-button' : 'room-button';
-          const buttonText = room === props.roomNameConfirmed ? 'Joined' : 'Join';
-          return(
-            <div key={room} className={containerClass}>
-              <div className='room-list-text'>{room}</div>
-              <button onClick={(e)=>props.handleJoinRoomClick(e, room)} className={buttonClass}>{buttonText}</button>
-            </div>
-          )
-        })}
-      </div>
+			<RoomWithJoinButton
+				key='roomsFiltered'
+				containerKey='roomsFiltered'
+				containerClass='rooms-filtered'
+				rooms={props.roomsFiltered}
+				roomNameConfirmed={props.roomNameConfirmed}
+				handleJoinRoomClick={props.handleJoinRoomClick}
+			/>
     )
   }
   if(Array.isArray(props.roomsRemaining) && props.roomsRemaining.length !== 0) {
     roomList.push(
-      <div key='roomsRemaining' className='rooms-remaining'>
-        {props.roomsRemaining.map((room) => {
-          const containerClass = room === props.roomNameConfirmed ? 'confirmed-room room-list' : 'room-list';
-          const buttonClass = room === props.roomNameConfirmed ? 'confirmed-room-button' : 'room-button';
-          const buttonText = room === props.roomNameConfirmed ? 'Joined' : 'Join';
-          return(
-            <div key={room} className={containerClass}>
-              <div className='room-list-text'>{room}</div>
-              <button onClick={(e)=>props.handleJoinRoomClick(e, room)}  className={buttonClass}>{buttonText}</button>
-            </div>
-          )
-        })}
-      </div>
+			<RoomWithJoinButton
+				key='roomsRemaining'
+				containerKey='roomsRemaining'
+				containerClass='rooms-remaining'
+				rooms={props.roomsRemaining}
+				roomNameConfirmed={props.roomNameConfirmed}
+				handleJoinRoomClick={props.handleJoinRoomClick}
+			/>
     )
   }
   if(Array.isArray(roomList) && roomList.length !== 0){
@@ -45,6 +55,7 @@ export const RoomList = (props) => {
     return <h4 className='room-text'>No rooms yet</h4>
   }
 }
+
 export const RoomInput = (props) => {
   const rankStringArray = (target, strings) => {
     if(Array.isArray(strings) && strings.length !== 0){
