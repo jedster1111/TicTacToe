353233b926ba67362b8bf309131e0a716cb67912
Jed <jedster1111@hotmail.co.uk>
2018-08-14 11:42:16 +0100

    removed comments from tests we were doing

--- src/server/server.js
+++ src/server/server.js
@@ -302,8 +302,7 @@ io.on("connection", client => {
 //    console.log(allPlayers.map(player => player.name));
 //    console.log(rooms);
 //}, 5000);
-// a comment it aborts
-// can you get this in your code
+
 process.on("SIGINT", function() {
   console.log("\nShutting down, some one hit ctrl c");
   console.log("Stopping Server...");
